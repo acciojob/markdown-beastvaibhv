2403457519b8b22913141a2a893305d9fe9f2ca9
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-12-20 08:09:12 +0000

    markdown

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,31 @@ import React, { useState, useEffect } from 'react';
 import "../styles/App.css";
 
 const App = () => {
-    const [text, setText] = useState("");
+    const [markdown, setMarkdown] = useState("");
     const [preview, setPreview] = useState("");
 
     useEffect(() => {
-        setPreview(text);
-    }, [text])
+        setPreview(markdown);
+    }, [markdown])
 
 
     return (
         <div >
             <div className='app'>
                 <div className='textarea'>
-                    <h1>Text Area</h1>
-                    <div >
+        
+                    
                     <textarea
                         className='textarea'
                         placeholder=''
-                        value={text}
-                        onChange={(e)=>setText(e.target.value)}
-                    >#</textarea>
-                    </div>
+                        value={markdown}
+                        onChange={(e)=>setMarkdown(e.target.value)}
+                    ></textarea>
+                    
                     
                 </div>
                 <div className='preview'>
-                    <h1>Preview</h1>
+            
                     <div className='loading'>{preview}</div>
                 </div>
 
